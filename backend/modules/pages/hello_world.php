ad36fae2dfde0b5060b694621da69f57b9fd62d3
Sooraj <soorajnraju@gmail.com>
2021-02-07 12:52:27 +0530

    admin menu

--- backend/modules/pages/hello_world.php
+++ backend/modules/pages/hello_world.php
@@ -10,10 +10,14 @@ namespace Snow\Backend\Modules\Pages;
 class Hello_World {
 
     public function __construct() {
-        add_menu_page( 'Hello', 'Hello', 'manage_options', 'hello_world', [$this, 'hello']);
+        add_action( 'admin_menu', [$this, 'hello'] );
     }
 
-    public function hello() {
+    public function hello(){
+        add_menu_page( 'Hello', 'Hello', 'manage_options', 'hello', [$this, 'hello_world'], 'dashicons-welcome-widgets-menus', 100);
+    }
+
+    public function hello_world() {
         ?>
         <h1>Hello World</h1>
         <?php
