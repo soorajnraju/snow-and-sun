87ecd4115c069c65ca8e3f4715a86a5c00220858
Sooraj <soorajnraju@gmail.com>
2021-02-07 13:15:40 +0530

    some cool things

--- snow-and-sun.php
+++ snow-and-sun.php
@@ -16,15 +16,34 @@ defined('ABSPATH') or exit('Direct access is not allowed');
 define('PLUGIN_PATH', plugin_dir_path( __FILE__));
 define('PLUGIN_NAME', 'snow-and-sun');
 
-require 'vendor/autoload.php';
+/**
+ * Load composer autoloader
+ */
+if(file_exists('vendor/autoload.php')){
+    require 'vendor/autoload.php';
+}
+
+/**
+ * Load custom registered autoloader
+ */
 require 'loader.php';
 new Loader();
+
+/**
+ * Load globals
+ */
 require 'global/constants.php';
 require 'global/functions.php';
 
+/**
+ * Create bootstrap modules instances
+ */
 $frontend = new Frontend\Bootstrap();
 $backend = new Backend\Bootstrap();
 
+/**
+ * Call init on plugins_loaded
+ */
 add_action('plugins_loaded', [$frontend, 'init']);
 add_action('plugins_loaded', [$backend, 'init']);
 
